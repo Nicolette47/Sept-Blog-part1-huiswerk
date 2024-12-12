57aa160f3719fffbcdcbd9d8868ed4a65f9ee2b8
nvanzeijl <nvanzeijl@gmail.com>
2024-12-12 18:22:33 +0100

    opdracht 2 start gemaakt

--- src/pages/overviewposts/OverviewPosts.jsx
+++ src/pages/overviewposts/OverviewPosts.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './OverviewPosts.css';
+import data from '../../constants/data.json';
 
 function OverviewPosts () {
+    console.log(data);
+
     return (
+        <>
         <h1>Overview alle blogs</h1>
+        <p>Aantal blogs : {data.length}</p>
+
+
+        </>
     );
 }
 
